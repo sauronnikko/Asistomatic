225b3be8ff2347de75ecc425099edd6814bed533
Nicolas Munoz <sauronnikko@gmail.com>
2011-05-20 18:18:37 -0430

    Commit introductorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asistomatic::Application.config.secret_token = 'd01b7b36323d96ba4f79fc0dd3558cdf5536117b0a26883fef4c68af8604ab805d04529396104d8a1d3fb982002185a3efe11f38edab71b6e963838111d6c6a9'
